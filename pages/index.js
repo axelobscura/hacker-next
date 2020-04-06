21efcb9c2f1d20dfcc7bcd2fcc762efe243147c8
Axel Obscura <axosar@gmail.com>
2020-04-06 12:56:59 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+class Index extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>HOME</h1>
+            </div>
+        )
+    }
+}
+
+export default Index;
