7c386ae058a2ae5b4beee628b379177959c03ac1
Axel Obscura <axosar@gmail.com>
2020-04-08 13:19:40 -0500

    Adding comment, HTML Unsafe, and global styles

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5d75f2bebb15fada8f91";
+/******/ 	var hotCurrentHash = "a4cd5077d68e12500c20";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
